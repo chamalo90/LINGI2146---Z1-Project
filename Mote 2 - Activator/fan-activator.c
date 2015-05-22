981cd54979a360bb22147d707326c27ef812066c
Matthieu Baerts <matttbe@gmail.com>
2015-05-22 18:28:11 +0200

    Fan: add comments + uniform style

--- Mote 2 - Activator/fan-activator.c
+++ Mote 2 - Activator/fan-activator.c
@@ -31,23 +31,25 @@
 #endif
 
 
-#define SERVER_NODE(ipaddr)   uip_ip6addr(ipaddr, 0xaaaa, 0, 0, 0, 0xc30c, 0, 0, 0x00c3) 
+#define SERVER_NODE(ipaddr)   uip_ip6addr(ipaddr, 0xaaaa, 0, 0, 0, 0xc30c, 0, 0, 0x00c3)
 
 #define LOCAL_PORT      UIP_HTONS(COAP_DEFAULT_PORT+1)
 #define REMOTE_PORT     UIP_HTONS(COAP_DEFAULT_PORT)
 #define HISTORY 4
 #define DEFAULT_TRESHOLD 25.0f
 
+// We want to keep temperature and time
 struct temp_record {
     int temperature;
-    unsigned long time;
+    unsigned long time; // we will not use time but it is useful when debugging + stats
 };
 static struct temp_record temperature[HISTORY];
 static int temp_pos;
-static int rssi;
-static int treshold;
-static int fan_frequency = 1;
-static struct etimer activator_timer;
+
+static int rssi; // Last RSSI value
+static int treshold; // can be modified
+static int fan_frequency = 1; // check each seconds
+static struct etimer activator_timer; // timer for the fan
 
 
 PROCESS(activator, "Fan Activator");
@@ -61,10 +63,14 @@ AUTOSTART_PROCESSES(&er_observe_client, &rest_server, &activator);
 
 /*----------------------------------------------------------------------------*/
 /*
- * RSSI Scanner
+ * Utils methods
  */
 /*----------------------------------------------------------------------------*/
 
+
+/*
+ * Store last RSSI value from radio sensor
+ */
 static void
 do_rssi(void)
 {
@@ -73,6 +79,10 @@ do_rssi(void)
 }
 
 static void get_temperature_and_time(char * json, struct temp_record * r)  
+/*
+ * Parse `json` string and put temperature and timestamp into `record` structure
+ * No need to use Contiki JSON parser just for that.
+ */
 {
   // { "temperature":21, "time":2412 }
   //Find the first ':'  and point to the next char
@@ -89,18 +99,22 @@ static void get_temperature_and_time(char * json, struct temp_record * r)
   r->time = atol(tmpchar); //convert it to long
 }
 
-static int mean(void) 
+/*
+ * Compute the mean temperature from all stored values
+ */
+static int
+mean(void)
 {
   int i;
   int mean = 0;
-  for(i = 0; i < HISTORY; i++) {
+  for(i = 0; i < HISTORY; i++)
     mean += temperature[i].temperature;
-  }
-  if (temp_pos < HISTORY) {
-    mean = mean / temp_pos;
-  }
-  else mean = mean / HISTORY;
-  return mean;
+
+  // if not enough info, use temp_pos
+  if (temp_pos < HISTORY)
+    return mean / temp_pos;
+  else
+    return mean / HISTORY;
 }
 
 /*----------------------------------------------------------------------------*/
@@ -109,14 +123,12 @@ static int mean(void)
  */
 /*----------------------------------------------------------------------------*/
 
-static uip_ipaddr_t server_ipaddr[1]; /* holds the server ip address */
+static uip_ipaddr_t server_ipaddr[1]; // holds the server ip address
 static coap_observee_t *obs;
 
 
- /* The path of the resource to observe */
-#define OBS_RESOURCE_URI "temperature/push"
+#define OBS_RESOURCE_URI "temperature/push" // resource to observate
 
-/*----------------------------------------------------------------------------*/
 /*
  * Handle the response to the observe request and the following notifications
  */
@@ -135,22 +147,26 @@ notification_callback(coap_observee_t *obs, void *notification,
   switch(flag) {
   case NOTIFICATION_OK:
     printf("NOTIFICATION OK: %*s\n", len, (char *)payload);
-    do_rssi();
-    struct temp_record * r = &temperature[(temp_pos++)%HISTORY];
-    get_temperature_and_time((char *)payload, r);
-    PRINTF("Readed Temp: %d, Readed Time %lu\n", r->temperature, r->time);
+    do_rssi(); // record last RSSI value
+    struct temp_record * record = &temperature[(temp_pos++)%HISTORY];
+    get_temperature_and_time((char *)payload, record);
+    printf("Readed Temp: %d, Readed Time %lu\n", record->temperature, record->time);
     break;
+
   case OBSERVE_OK: /* server accepeted observation request */
     printf("OBSERVE_OK: %*s\n", len, (char *)payload);
     break;
+
   case OBSERVE_NOT_SUPPORTED:
     printf("OBSERVE_NOT_SUPPORTED: %*s\n", len, (char *)payload);
     obs = NULL;
     break;
+
   case ERROR_RESPONSE_CODE:
     printf("ERROR_RESPONSE_CODE: %*s\n", len, (char *)payload);
     obs = NULL;
     break;
+
   case NO_REPLY_FROM_SERVER:
     printf("NO_REPLY_FROM_SERVER: "
            "removing observe registration with token %x%x\n",
@@ -159,21 +175,23 @@ notification_callback(coap_observee_t *obs, void *notification,
     break;
   }
 }
-/*----------------------------------------------------------------------------*/
+
 /*
  * Toggle the observation of the remote resource
  */
 void
 toggle_observation(void)
 {
-  if(obs) {
+  if (obs) {
     printf("Stopping observation\n");
     coap_obs_remove_observee(obs);
     obs = NULL;
-  } else {
+  }
+  else {
     printf("Starting observation\n");
     obs = coap_obs_request_registration(server_ipaddr, REMOTE_PORT,
-                                        OBS_RESOURCE_URI, notification_callback, NULL);
+                                        OBS_RESOURCE_URI, notification_callback,
+                                        NULL);
   }
 }
 
@@ -191,7 +209,7 @@ PROCESS_THREAD(er_observe_client, ev, data)
   /* receives all CoAP responses */
   coap_init_engine();
   temp_pos = 0;
-  
+
   toggle_observation();
 
   while(1) {
@@ -221,7 +239,7 @@ res_post_handler(void *request, void *response, uint8_t *buffer, uint16_t prefer
 {
   size_t len = 0;
   const char *tresh = NULL;
-  if((len = REST.get_post_variable(request, "treshold", &tresh))) {
+  if ((len = REST.get_post_variable(request, "treshold", &tresh))) {
     treshold = atoi(tresh);
   }
 }
@@ -242,8 +260,8 @@ PROCESS_THREAD(rest_server, ev, data)
   rest_activate_resource(&res_toggle, "treshold");
 
   while(1) {
-    PROCESS_WAIT_EVENT();
-  }        
+    PROCESS_WAIT_EVENT(); // Wait for instructions
+  }
 
   PROCESS_END();
 }
@@ -259,23 +277,25 @@ PROCESS_THREAD(activator, ev, data)
 {
   PROCESS_BEGIN();
   treshold = DEFAULT_TRESHOLD;
-  
+
   etimer_set(&activator_timer, CLOCK_SECOND / fan_frequency);
-  static int state = 0;
+  static int state = 0; // LED off
   while(1) {
     PROCESS_WAIT_EVENT();
      if(etimer_expired(&activator_timer)) {
       int mean_value = mean();
       int delta = (mean_value - treshold) * (2 - rssi/100);
       if (delta > 0) {
-        fan_frequency = delta ;
-        if (delta > 7) delta = 7;
+        fan_frequency = delta;
+        if (delta > 7) delta = 7; // max value = 7 (3 bits)
       }
       else {
-        fan_frequency = 1;
+        fan_frequency = 1; // check min each second
         delta = 0;
       }
-      if (state == 1)  {
+
+      // Toogle LEDS
+      if (state == 1) {
         leds_off(LEDS_ALL);
         state = 0;
       }
@@ -283,10 +303,12 @@ PROCESS_THREAD(activator, ev, data)
         leds_on(delta);
         state = 1;
       }
+
+      // Print for measurements
       PRINTF("Fan Frq: %d, Delta: %d, Threshold: %d, Mean: %d, RSSI: %d \n", fan_frequency, delta, treshold, mean_value, rssi);
-      etimer_set(&activator_timer, CLOCK_SECOND / fan_frequency);
+      etimer_set(&activator_timer, CLOCK_SECOND / fan_frequency); // Adapt frequency: blink led
     }
-  }       
-  
+  }
+
   PROCESS_END();
 }
